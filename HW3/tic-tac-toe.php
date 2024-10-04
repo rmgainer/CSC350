7a1b966ef1e0a97ff34ffa4916ca97066accc1e2
rmgainer <66702450+rmgainer@users.noreply.github.com>
2024-10-04 22:44:02 +0000

    added draw condition, colors, play again button if game over

--- HW3/tic-tac-toe.php
+++ HW3/tic-tac-toe.php
@@ -1,23 +1,41 @@
-<?php session_start(); ?>
+<?php 
+    session_start();
+?>
 <!doctype html>
 <html lang="en">
 <head>
-
+    <title>Tic Tac Toe</title>
+    <h1>Tic Tac Toe</h1>
 </head>
 <body>
 <?php 
     include 'tic-tac-toe-functions.php'; 
 
-    function printboard($whoseturn) {
+    $count = 0;
+    $whoseturn = 'X';
+
+    function printboard($whoseturn, $gameover) {
+        if (!$gameover) {
+            print "<p>Turn: " . $whoseturn . "</p>";
+        }
         print "<table><form method='post' action='tic-tac-toe.php'>";
         for ($i = 1; $i <= 3; $i++) {
             print "<tr>";
             for ($j = 1; $j <= 3; $j++) {
-                print "<td>";
+                print "<td";
                 if ($_SESSION["$j-$i"]) {
+                    if ($_SESSION["$j-$i"] == 'X') {
+                        print " style='background-color:green;'>";
+                    } else {
+                        print " style='background-color:red;'>";
+                    }
                     print $_SESSION["$j-$i"];
                 } else {
-                    print "<input type='submit' name='$j-$i' value='$whoseturn'>";
+                    if (!$gameover) {
+                        print "><input type='submit' name='$j-$i' value='$whoseturn'>";
+                    } else {
+                        print " ";
+                    }
                 }
                 print "</td>";
             }
@@ -26,6 +44,14 @@
         print "</form></table>";
     }
 
+    function restart($whoseturn) {
+        $gameover = true;
+        printboard($whoseturn, true);
+        $_SESSION = array();
+        session_destroy();
+        print "<p><form method='post' action='tic-tac-toe.php'>
+               <input type='submit' value='Play again'></form></p>";
+    }
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         for ($i = 1; $i <= 3; $i++) {
@@ -34,23 +60,24 @@
                     $_SESSION["$j-$i"] = $_POST["$j-$i"];
                     if ($_SESSION["$j-$i"] == 'X'){
                         $whoseturn = 'O';
-                    } else {
-                        $whoseturn = 'X';                 
                     }
+                }
+                if (isset($_SESSION["$j-$i"])) {
+                    $count++;
                 }
             }
         }
-
-    } else {
-        $whoseturn = 'X';
     }
+
     $winner = whoIsWinner();
     if ($winner != null) {
-        print "The winner is " . $winner . "!";
+        print "<p>The winner is " . $winner . "!!</p>";
+        restart($whoseturn);
     } else if ($count == 9) {
-        print "It's a draw";
+        print "<p>It's a draw.</p>";
+        restart($whoseturn);
     } else {
-        printboard($whoseturn);
+        printboard($whoseturn, false);
     }
 
 ?>
