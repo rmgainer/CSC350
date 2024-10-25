d3cca624d60a0422a2839b2560485c7f881ec606
rmgainer <66702450+rmgainer@users.noreply.github.com>
2024-10-25 14:28:07 -0400

    added code to update database with entered form data

--- HW5/entergrades.php
+++ HW5/entergrades.php
@@ -7,17 +7,53 @@
 <body>
     
 <?php
-    require_once "config.php";
+    require_once "config.php"; //connect to db
+
     $query1 = "SELECT * FROM Students ORDER BY lastName";
     $students = mysqli_query($conn, $query1);
+
     $query2 = "SELECT * FROM Assignments";
     $assignments = mysqli_query($conn, $query2);
+
+    $error = false;
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') { //if form is submitted
+        $studentID = $_POST['studentID'];
+        foreach ($_POST['grades'] AS $num => $grade) {
+            $grades[$num + 1] = $grade; //since autoinc starts at 1
+        }
+        $query = "SELECT * FROM Scores WHERE studentID = $studentID;";
+        $result = mysqli_query($conn, $query);
+        if ($result->num_rows > 0) { //if student already has grades entered
+            //while ($row = mysqli_fetch_array($assignments)) {
+            foreach ($grades AS $assignmentID => $grade) {
+                $sql = "UPDATE Scores SET grade = $grade WHERE studentID = $studentID AND assignmentID = $assignmentID;";  
+                if (!($result = mysqli_query($conn, $sql))) {
+                    $error = true;
+                }
+            } 
+        } else { //if student does not exist in Scores table
+            //while ($row = mysqli_fetch_array($assignments)) {
+            foreach ($grades AS $assignmentID => $grade) {
+                $sql = "INSERT INTO Scores (studentID, assignmentID, grade)
+                VALUES ('$studentID', '$assignmentID', '$grade');";
+                if (!($result = mysqli_query($conn, $sql))) {
+                    $error = true;
+                }
+            }
+        }
+        if ($error) {
+            print "<p>ERROR SOMETHING IS WRONG</p>";
+        } else {
+            print "<p>Grades successfully submitted.</p>";
+        }
+    }
 ?>
 
 <div><p>Please select a student and enter their scores.</p></div>
-    <form method = "post" action = "submitgrades.php">
+    <form method = "post" action = "entergrades.php">
     <table>
-        <tr><td><label for="studentID">Select student: </label>
+        <tr><td><label for="studentID">Select student: </label> 
         <td><select name="studentID" id="studentID" required>
         <?php while ($row = mysqli_fetch_array($students)) : ?>
             <option value="<?php print $row['studentID']; ?>">
